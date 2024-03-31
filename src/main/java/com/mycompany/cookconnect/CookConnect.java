a9a74c837157ff8297a9d4d3b772322315a57101
lodi432 <glavacevic.d@gmail.com>
2024-03-31 21:31:01 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/cookconnect/CookConnect.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cookconnect;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ *
+ * @author glava
+ */
+@SpringBootApplication
+
+public class CookConnect {
+
+    public static void main(String[] args) {
+    SpringApplication.run(CookConnect.class, args);        
+//System.out.println("Hello World!");
+    }
+}
